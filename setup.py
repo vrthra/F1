2dc23db05cbf01846c9b59642d6f3e5cc619770f
Rahul Gopinath <rahul@gopinath.org>
2019-11-18 08:10:54 -0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="F1-Fuzzer", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Rahul Gopinath",
     author_email="rahul@gopinath.org",
     description="A fast grammar fuzzer",
