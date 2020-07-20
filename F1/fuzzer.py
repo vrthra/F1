b70e03e9a8993e56d597e9c5124bca4dc83d021a
Rahul Gopinath <vrthra@users.noreply.github.com>
2020-07-20 10:30:30 +0200

    Add a non recursive version

--- F1/fuzzer.py
+++ F1/fuzzer.py
@@ -119,6 +119,53 @@ class LimitFuzzer(LimitFuzzer):
                 cost[k][str(rule)] = self.expansion_cost(grammar, rule, set())  
         return cost
 
+    
+# A non recursive version.
+class LimitFuzzer_NR(LimitFuzzer):
+    def is_nt(self, name):
+        return (name[0], name[-1]) == ('<', '>')
+ 
+    def tree_to_str(self, tree):
+        name, children = tree
+        if not self.is_nt(name): return name
+        return ''.join([self.tree_to_str(c) for c in children])
+ 
+    def nonterminals(self, rule):
+        return [t for t in rule if self.is_nt(t)]
+
+    def gen_key(self, key, max_depth):
+        def get_def(t):
+            if self.is_nt(t):
+                return [t, None]
+            else:
+                return [t, []]
+
+        cheap_grammar = {}
+        for k in self.cost:
+            # should we minimize it here? We simply avoid infinities
+            rules = self.grammar[k]
+            min_cost = min([self.cost[k][str(r)] for r in rules])
+            #grammar[k] = [r for r in grammar[k] if self.cost[k][str(r)] == float('inf')]
+            cheap_grammar[k] = [r for r in self.grammar[k] if self.cost[k][str(r)] == min_cost]
+
+        root = [key, None]
+        queue = [(0, root)]
+        while queue:
+            # get one item to expand from the queue
+            (depth, item), *queue = queue
+            key = item[0]
+            if item[1] is not None: continue
+            grammar = self.grammar if depth < max_depth else cheap_grammar
+            chosen_rule = random.choice(grammar[key])
+            expansion = [get_def(t) for t in chosen_rule]
+            item[1] = expansion
+            for t in expansion: queue.append((depth+1, t))
+            #print("Fuzz: %s" % key, len(queue), file=sys.stderr)
+        #print(file=sys.stderr)
+        return root
+
+    def fuzz(self, key='<start>', max_depth=10):
+        return self.tree_to_str(self.gen_key(key=key, max_depth=max_depth))
 
 class PooledFuzzer(LimitFuzzer):
     def compute_cost(self, grammar, cost={}):
